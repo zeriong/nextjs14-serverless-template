db610b8a6559be59c493bd4a1373c50778abda86
jaery <jaeryong95@gmail.com>
2024-12-14 00:57:29 +0900

    feat: dnd-kit를 활용한 예시 컴포넌트 설계

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import DndVerticalSortableContext from "@/app/_components/dnd/DndVerticalSortableContext";
+import DndVerticalSortableItem from "@/app/_components/dnd/DndVerticalSortableItem";
+import Image from "next/image";
 
 export default function Page() {
-  // const test = { sss: "sddasf", seer: 123 };
+  const [items, setItems] = useState([{ name: "item" }, { name: "item2" }]);
 
   return (
     <div className="p-[30px]">
       <p>관리자페이지</p>
       <Link href="/">홈으로</Link>
+
+      <div className={"mt-4 bg-gray-400 p-3"}>
+        <DndVerticalSortableContext items={items} setItems={setItems}>
+          {items?.map((item) => (
+            <DndVerticalSortableItem key={item.name}>
+              {({ attributes, listeners }) => (
+                <div>
+                  <div
+                    {...attributes}
+                    {...listeners}
+                    className=""
+                    tabIndex={-1} // 텝인덱스 방지
+                  >
+                    <Image src={"/assets/svg/drag_handle_icon.svg"} alt="drag_handle_icon" width={14} height={14} />
+                  </div>
+                </div>
+              )}
+            </DndVerticalSortableItem>
+          ))}
+        </DndVerticalSortableContext>
+      </div>
     </div>
   );
 }
